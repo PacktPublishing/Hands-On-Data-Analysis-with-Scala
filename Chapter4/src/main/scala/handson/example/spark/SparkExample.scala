159f4446fe85b893d2cfb989c58ce40ef87f65e3
Rajesh Gupta <rogers_gupta@yahoo.com>
2018-12-03 00:21:09 -0800

    Added Spark + Vegas Viz Example

--- Chapter4/src/main/scala/handson/example/spark/SparkExample.scala
+++ Chapter4/src/main/scala/handson/example/spark/SparkExample.scala
@@ -2,7 +2,13 @@ package handson.example.spark
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkFiles
+import vegas._
+import vegas.sparkExt._
 
+
+/**
+  * Example to demonstrate Spark and Vegas Viz usage
+  */
 object SparkExample {
   def getSparkSession(): SparkSession = {
     val spark = SparkSession.builder().master("local").getOrCreate()
@@ -11,13 +17,20 @@ object SparkExample {
   }
   def main(args: Array[String]): Unit = {
     val spark = getSparkSession()
+
     spark.sparkContext.addFile("https://data.lacity.org/api/views/nxs9-385f/rows.csv")
     val df = spark.read.option("header", true).option("inferSchema", true).csv(SparkFiles.get("rows.csv"))
     df.printSchema()
     df.show()
-    df.sample(0.05).show() // 5% random sample
     println("Covariance: " + df.stat.cov("Total Population", "Total Households"))
     println("Correlation: " + df.stat.corr("Total Population", "Total Households"))
+    df.createOrReplaceTempView("tmp_data")
+    val dfWithTier = spark.sql("select *, ntile(100) over(order by `Total Population`) tier from tmp_data")
+    val dfTier90Plus = dfWithTier.where("tier >= 90")
+    val plot = Vegas().withDataFrame(dfTier90Plus).encodeX("Zip Code", Nom).
+      encodeY("Total Population", Quant).
+      mark(Bar)
+    plot.show
 
     spark.stop()
   }

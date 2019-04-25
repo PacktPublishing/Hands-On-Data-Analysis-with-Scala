8c970f1db1a46f7f1fc6378d89656133d1bbb2b7
Rajesh Gupta <rogers_gupta@yahoo.com>
2019-04-25 00:01:58 -0700

    Added more Chapter 7 example code

---
+++ Chapter7/src/main/scala/handson/example/LinearRegExample.scala
@@ -0,0 +1,49 @@
+package handson.example
+
+import org.apache.spark.sql.SparkSession
+
+object LinearRegExample {
+  val homeDir = System.getProperty("user.home")
+  def main(args: Array[String]): Unit = {
+    // 1. Set Spark session
+    val spark = SparkSession.builder().master("local").getOrCreate()
+
+    // 2. Set logging level to WARNING
+    spark.sparkContext.setLogLevel("WARN")
+
+    // 3. Import necessary classes from Spark MLLib package that are needed for linear regression
+    import org.apache.spark.mllib.linalg.Vectors
+    import org.apache.spark.mllib.regression.LabeledPoint
+    import org.apache.spark.mllib.regression.LinearRegressionModel
+    import org.apache.spark.mllib.regression.LinearRegressionWithSGD
+
+    // 4. Load the data
+    val data = spark.sparkContext.textFile(s"${homeDir}/lpsa.data")
+    // 5. Parse the data into LabeledPoint and cache
+    val parsedData = data.map { line =>
+      val parts = line.split(',')
+      LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(' ').map(_.toDouble)))
+    }.cache()
+
+    // 6. Build the model by setting number of iterations, step size
+    val numIterations = 100
+    val stepSize = 0.00000001
+    val model = LinearRegressionWithSGD.train(parsedData, numIterations, stepSize)
+
+    // 7. Evaluate model on training examples and compute training error
+    val valuesAndPreds = parsedData.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
+    val MSE = valuesAndPreds.map{ case(v, p) => math.pow((v - p), 2) }.mean()
+    println(s"training Mean Squared Error $MSE")
+
+    // 8. Save the model
+    model.save(spark.sparkContext, s"{homeDir}/LinearRegressionWithSGDModel")
+    // 9. Load the saved model
+    val sameModel = LinearRegressionModel.load(spark.sparkContext, s"{homeDir}/LinearRegressionWithSGDModel")
+    // 10. Output the model
+    sameModel
+  }
+
+}
---
+++ Chapter7/src/main/scala/handson/example/RandomForestExample.scala
@@ -0,0 +1,64 @@
+package handson.example
+
+import org.apache.spark.sql.SparkSession
+
+object RandomForestExample {
+  val homeDir = System.getProperty("user.home")
+  def main(args: Array[String]): Unit = {
+    // a. Set Spark session
+    val spark = SparkSession.builder().master("local").getOrCreate()
+
+    // b. Set logging level to WARNING
+    spark.sparkContext.setLogLevel("WARN")
+
+    // 1. Import required classes from Spark's ML package
+    import org.apache.spark.ml.Pipeline
+    import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
+    import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+    import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+
+    // 2. Use Spark session to read the file sample_libsvm_data.txt in libsvm format as a Spark DataFrame
+    val data = spark.read.format("libsvm").load(s"${homeDir}/sample_libsvm_data.txt")
+
+    // 3. Index the label column of the source DataFrame and create a new DataFrame with indexedLabel as additional column
+    val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(data)
+
+    // 4. Index the feautures column of the source DataFrame and create a new DataFrame with indexedFeatures as additional column.
+    // Make sure that features is treated as constinuos variable if there are more than four distinct values else categorical
+    val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(data)
+
+    // 5. Randomly split the source DataFrame into two DataFrames with a 7 to 3 ratio. First DataFrmae is to be used for training purposes.
+    // Second DataFrame is to be used for testing purpose.
+    val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
+
+    // 6. Create a RandomForestClassifier using the builder pattern. Set label column name as indexedLabel and features coulmn name as features column.
+    // Allow 10 decision trees to be used for arriving at consensus
+    val randomForest = new RandomForestClassifier().setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures").setNumTrees(10)
+
+    // 7. Create a label coverter object that converts indexed labels back to original labels
+    val labelConverter = new IndexToString().setInputCol("prediction").setOutputCol("predictedLabel").setLabels(labelIndexer.labels)
+
+    // 8. Create a Pipeline object that chains label indexer, feature indexer, random forest and label converter.
+    val pipeline = new Pipeline().setStages(Array(labelIndexer, featureIndexer, randomForest, labelConverter))
+
+    // 9. Run the pipeline to fit the the training data and create a model. Doing so runs  label indexer, feature indexer, random forest and label coverter one after the other.
+    val model = pipeline.fit(trainingData)
+
+    // 10. Using the model run predictions on test data.
+    val predictions = model.transform(testData)
+
+    // 11. Display a two rows from above creeated predictions DataFrame.
+    predictions.select("predictedLabel", "label", "features").show(5, true)
+
+    // 12. Evalue the accuracy of predicted label by comparing agaist the original label.
+    val evaluator = new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(predictions)
+    println(s"Test Error = ${(1.0 - accuracy)}")
+
+    // 13. Extract the built model getting the element at index 2 of pipeline stages
+    val rfModel = model.stages(2).asInstanceOf[RandomForestClassificationModel]
+
+    // 14. Output contents of the built model
+    println(s"Learned classification forest model:\n ${rfModel.toDebugString}")
+  }
+}

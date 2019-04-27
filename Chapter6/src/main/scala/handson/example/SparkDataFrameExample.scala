78078b99228e5ecd26fd55f69344816a0142937e
Rajesh Gupta <rogers_gupta@yahoo.com>
2019-04-27 11:36:18 -0700

    Chapter 6: Spark DataFrame Example

---
+++ Chapter6/src/main/scala/handson/example/SparkDataFrameExample.scala
@@ -0,0 +1,42 @@
+package handson.example
+
+object SparkDataFrameExample {
+
+  def main(args: Array[String]): Unit = {
+    // 0a. Set Spark session
+    import org.apache.spark.sql.SparkSession
+    val spark = SparkSession.builder().master("local").getOrCreate()
+    import spark.implicits._
+
+    // 0b. Set logging level to WARNING
+    spark.sparkContext.setLogLevel("WARN")
+
+    // 1. Create a DataFrame of first few prime numbers
+    val df = spark.createDataFrame(Seq(
+      (2, "two"),
+      (3, "three"),
+      (5, "five"),
+      (7, "seven"),
+      (11, "eleven"),
+      (13, "thirteen"),
+      (17, "seventeen")
+    )).toDF("num", "description")
+
+    // 2. Get DataFrame size
+    val count = df.count()
+    println(count)
+
+    // 3. Compute total by using the reduce method of DataFrame
+    val total = df.selectExpr("sum(num) total").collect()(0)(0).asInstanceOf[Long]
+    println(total)
+
+
+    // 5. Use collect to bring DataFrame data to driver
+    val dfData = df.collect()
+    dfData.foreach(println)
+
+    // Stop Spark session before existing
+    spark.stop()
+  }
+
+}

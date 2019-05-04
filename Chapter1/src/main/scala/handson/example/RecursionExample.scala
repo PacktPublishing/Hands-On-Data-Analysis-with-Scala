a6d5ef6b4e0ac361435e3304ed0ac6addea9b691
Rajesh Gupta <rogers_gupta@yahoo.com>
2019-05-04 12:54:05 -0700

    Chapter 1: Added Palindrome Example with tail recursion

--- Chapter1/src/main/scala/handson/example/RecursionExample.scala
+++ Chapter1/src/main/scala/handson/example/RecursionExample.scala
@@ -4,6 +4,8 @@ import scala.annotation.tailrec
 
 /**
   * Recursion example using standard and tail recursion
+  * [[https://en.wikipedia.org/wiki/Factorial Factorial Wikipedia]]
+  * [[https://en.wikipedia.org/wiki/Palindrome Palindrome Wikipedia]]
   */
 object RecursionExample {
 
@@ -19,8 +21,26 @@ object RecursionExample {
     factorialAcc(1, n)
   }
 
+  /**
+    * Is Palindrome?
+    * @param s input string
+    * @return true if input is Palindrome else false
+    */
+  @tailrec
+  def isPalindrome(s: String): Boolean = {
+    require(s != null)
+    if (s.isEmpty || s.length == 1)
+      true
+    else if (s(0) != s(s.length-1))
+      false
+    else
+       isPalindrome(s.tail.substring(s.length-1))
+  }
+
   def main(args: Array[String]): Unit = {
-    println(factorial(10))
-    println(optimizedFactorial(10))
+    println(factorial(10)) // Expected: 3628800
+    println(optimizedFactorial(10)) // Expected: 3628800
+    println(isPalindrome("abcdedcba")) // Expected: true
+    println(isPalindrome("abcdedcbae")) // Expected: false
   }
 }

46713d3fec18f3894ad70a939d2ba07b44bda1bc
Rajesh Gupta <rogers_gupta@yahoo.com>
2019-05-05 10:36:08 -0700

    Chapter 1: Simplified Palindrome Example with tail recursion

--- Chapter1/src/main/scala/handson/example/RecursionExample.scala
+++ Chapter1/src/main/scala/handson/example/RecursionExample.scala
@@ -4,16 +4,25 @@ import scala.annotation.tailrec
 
 /**
   * Recursion example using standard and tail recursion
+  *
   * [[https://en.wikipedia.org/wiki/Factorial Factorial Wikipedia]]
   * [[https://en.wikipedia.org/wiki/Palindrome Palindrome Wikipedia]]
   */
 object RecursionExample {
-
-  // standard recursion
+  /**
+    * Factorial of a number
+    * @param n input number
+    * @return factorial value
+    */
   def factorial(n: Int): Long = if (n <= 1) 1 else n * factorial(n-1)
 
-  // tail recursion
-  def optimizedFactorial(n: Int): Long = {
+  /**
+    * Factorial of a number using tail recursion optimization
+    * @param n input number
+    * @return factorial value
+    */
+  def factorialTailRec(n: Int): Long = {
+    require(n > 0)
     @tailrec
     def factorialAcc(acc: Long, i: Int): Long = {
       if (i <= 1) acc else factorialAcc(acc * i, i -1)
@@ -22,25 +31,32 @@ object RecursionExample {
   }
 
   /**
+    * Strip out first and last character from a string
+    * @param s input string
+    * @return stripped out string
+    */
+  private[this] def stripOutFirstAndLastChar(s: String) = s.tail.dropRight(1)
+
+  /**
     * Is Palindrome?
     * @param s input string
     * @return true if input is Palindrome else false
     */
   @tailrec
-  def isPalindrome(s: String): Boolean = {
+  def isPalindromeTailRec(s: String): Boolean = {
     require(s != null)
     if (s.isEmpty || s.length == 1)
       true
-    else if (s(0) != s(s.length-1))
+    else if (s.charAt(0) != s.charAt(s.length-1))
       false
     else
-       isPalindrome(s.tail.substring(s.length-1))
+       isPalindromeTailRec(s.tail.substring(s.length-1))
   }
 
   def main(args: Array[String]): Unit = {
     println(factorial(10)) // Expected: 3628800
-    println(optimizedFactorial(10)) // Expected: 3628800
-    println(isPalindrome("abcdedcba")) // Expected: true
-    println(isPalindrome("abcdedcbae")) // Expected: false
+    println(factorialTailRec(10)) // Expected: 3628800
+    println(isPalindromeTailRec("abcdedcba")) // Expected: true
+    println(isPalindromeTailRec("abcdedcbae")) // Expected: false
   }
 }
